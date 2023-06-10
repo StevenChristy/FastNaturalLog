1041661f1522ea6e8b650bbf0c1089b731ea739e
Steven Christy <steven.l.christy@gmail.com>
2023-06-10 00:53:59 -0500

    Formatting changes and included license in source file.

--- FNL.cpp
+++ FNL.cpp
@@ -1,3 +1,32 @@
+/*
+Fast Natural Log - https://github.com/StevenChristy/FastNaturalLog
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -263,20 +292,20 @@ FP64 NaturalLog(FP64 x)
 }
 
 
-int main() {
-  double tests[] = {std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::quiet_NaN(),
-    	-2.0, -1.5, -1., -0.5, 0.0, 0.0000001, 0.01, 0.05, 0.1, 0.2, 0.5, 0.6, 0.85, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2., 2.1, 2.2, 100.0, 1000.0, 10000.0};
+int main() 
+{
+	double tests[] = {std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::quiet_NaN(),
+    		-2.0, -1.5, -1., -0.5, 0.0, 0.0000001, 0.01, 0.05, 0.1, 0.2, 0.5, 0.6, 0.85, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2., 2.1, 2.2, 100.0, 1000.0, 10000.0};
 
 	std::cout << std::setw(10) << " " << std::setw(11) << std::right << "FP32" << std::setw(11) << "" << std::setw(18) << std::right << "FP64" << std::endl; 
 	std::cout << std::setw(10) << std::right << "LN" << std::setw(11) << std::right << "FNL" << std::setw(11) << std::right << "STL" << std::setw(18) << std::right << "FNL" << std::setw(18) << std::right << "STL" << std::endl; 
 	
 	int numTests = sizeof(tests) / sizeof(double);
-    for (int i = 0; i < numTests; i++) {
-
-        float fr = NaturalLog(FP32(tests[i]));
+	for (int i = 0; i < numTests; i++) 
+	{
+        	float fr = NaturalLog(FP32(tests[i]));
 		double dr = NaturalLog(FP64(tests[i]));
 		std::cout << std::setprecision(6) << std::setw(10) << tests[i] << std::setw(11) << fr << std::setw(11) << std::log(static_cast<float>(tests[i])) << std::setprecision(12) << std::setw(18) << dr << std::setw(18) <<  std::log(tests[i]) << std::endl; 
-    }
-    
-    return 0;
+	}
+	return 0;
 }
